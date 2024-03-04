bbb36138ce38c5ad3e95995bcd49c0acec80322c
butterincup <143638043+butterincup@users.noreply.github.com>
2024-03-04 15:54:11 -0700

    Add files via upload

---
+++ practicas/prueba.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class prueba{
+	public static void main(String atgs[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("introduce tu nombre");
+		String nombre = sc.nextLine();
+		System.out.println("tu nombre es");
+		System.out.println(nombre);
+
+	}
+
+}
