8091dd0be1815cbd50f158fc16b7f90a35a348fb
butterincup <143638043+butterincup@users.noreply.github.com>
2024-04-15 15:16:24 -0700

    Add files via upload

---
+++ Eva2_19_for_2/src/eva2_19_for_2/Eva2_19_for_2.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_19_for_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Eva2_19_for_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Preguntar: cantidad de estudiantes en el grupo
+        int numAlum;
+        Scanner sc = new Scanner (System.in);
+        System.out.println("¿Cuantos estudiantes tiene el grupo?");
+        numAlum = sc.nextInt();
+        
+        for (int i = 1; i <= numAlum; i++) {
+            System.out.println("Calidicacion: ");
+            int califa = sc.nextInt();
+            
+        }
+{
+            
+        }
+        
+        
+    }
+    
+}
