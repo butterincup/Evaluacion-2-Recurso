d7fe50735651c10d069bb8e0f7a1c476dfdce2fa
butterincup <143638043+butterincup@users.noreply.github.com>
2024-04-15 21:03:40 -0700

    Add files via upload

---
+++ Eva2_22_Do_while/src/eva2_22_do_while/Eva2_22_Do_while.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_22_do_while;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Eva2_22_Do_while {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int cont=1;
+    do{
+        System.out.print(cont + " - ");
+        cont++;
+    }while(cont <= 10);
+    }
+    
+}
