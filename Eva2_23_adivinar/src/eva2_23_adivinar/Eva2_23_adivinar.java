d29b4a6a21dbea72917c980288c6f5b0445e6989
butterincup <143638043+butterincup@users.noreply.github.com>
2024-04-15 21:04:44 -0700

    Add files via upload

---
+++ Eva2_23_adivinar/src/eva2_23_adivinar/Eva2_23_adivinar.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_23_adivinar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Eva2_23_adivinar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num=5; //numero que tiene que adivinar
+        int valor = 0; //valor para almacenar lo que capture el usuario
+        Scanner sc=new Scanner (System.in);
+        do{
+            System.out.println("¿Cual es el numero? (entre el 1 al 10)");
+            valor=sc.nextInt();
+        }while(valor != num);
+        System.out.println("ADIVINASTE!!");
+    }
+    
+    
+}
