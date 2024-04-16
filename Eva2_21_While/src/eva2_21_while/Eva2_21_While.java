dd8fd7fddbc063ab3e1ae8c3206a77ff0892a291
butterincup <143638043+butterincup@users.noreply.github.com>
2024-04-15 21:02:10 -0700

    Add files via upload

---
+++ Eva2_21_While/src/eva2_21_while/Eva2_21_While.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_21_while;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Eva2_21_While {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*For --> Cuenta
+        WHILE & DO WHILE --> con condiciones
+        */
+        int cont=1;
+        while(cont <= 10){
+            System.out.print(cont + " - ");
+            cont++;
+        }
+        
+    }
+    
+}
